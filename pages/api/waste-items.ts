70e56b3076938d57686f1ed94a373987ec5dee40
ShiningElektra <elektrathanos@googlemail.com>
2021-03-17 14:16:16 +0100

    ✨Create API Routes

--- pages/api/waste-items.ts
+++ pages/api/waste-items.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import db from "../../server/db.json";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json(db["waste-items"]);
 };
