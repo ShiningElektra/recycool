32e7421ec778661362513fcfd46cbddc42e7125d
ShiningElektra <elektrathanos@googlemail.com>
2021-03-23 12:12:54 +0100

    🚧  Problem to setOpen state Issue #2

--- components/burger/Burger.tsx
+++ components/burger/Burger.tsx
@@ -1,15 +1,26 @@
 import { ButtonHTMLAttributes } from "react";
 import styles from "../burger/Burger.module.css";
+import React from "react";
+import { bool, func } from "prop-types";
 
 export type BurgerProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Burger = (props: BurgerProps) => {
+const Burger = ({ open, setOpen }, props: BurgerProps) => {
   return (
-    <button className={styles.burger} {...props}>
+    <button
+      className={styles.burger}
+      {...props}
+      open={open}
+      onClick={() => setOpen(!open)}
+    >
       <div></div>
       <div></div>
       <div></div>
     </button>
   );
+};
+Burger.propTypes = {
+  open: bool.isRequired,
+  setOpen: func.isRequired,
 };
 export default Burger;
--- components/menu/Menu.tsx
+++ components/menu/Menu.tsx
@@ -1,8 +1,15 @@
 import styles from "../menu/Menu.module.css";
+import React from "react";
+import { bool } from "prop-types";
 
-const Menu = () => {
+const Menu = ({ open }) => {
+  const className = "menu";
+  if (open) {
+    className += " open";
+  }
+
   return (
-    <nav className={`${styles.menu}`}>
+    <nav open={open} className={className}>
       <a href="/">
         <span role="img" aria-label="price">
           &#x1f4b8;
@@ -20,5 +27,8 @@ const Menu = () => {
       </a>
     </nav>
   );
+};
+Menu.propTypes = {
+  open: bool.isRequired,
 };
 export default Menu;
