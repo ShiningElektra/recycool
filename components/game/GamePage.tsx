64bc9e8a7b2e4b82ec846c59828299d8ef5c68cc
ShiningElektra <elektrathanos@googlemail.com>
2021-03-30 11:53:48 +0200

    ✨ Add cans component

--- components/game/GamePage.tsx
+++ components/game/GamePage.tsx
@@ -1,9 +1,13 @@
+import Cans from "../cans/Cans";
 import Field from "../field/Field";
 import styles from "./GamePage.module.css";
 
 const GamePage = () => {
   return (
     <div className={styles.container}>
+      <div className={styles.cans}>
+        <Cans />
+      </div>
       <div className={styles.fields}>
         <Field />
         <Field />
