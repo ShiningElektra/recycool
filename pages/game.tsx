8cae165e06df27fa1ea685d453729ac8709c8096
ShiningElektra <elektrathanos@googlemail.com>
2021-04-07 10:47:23 +0200

    ✨ 🚧  Add setTimeout

--- pages/game.tsx
+++ pages/game.tsx
@@ -2,8 +2,15 @@ import Head from "next/head";
 import Display from "../components/display/Display";
 import GamePage from "../components/game/GamePage";
 import styles from "../styles/Home.module.css";
+import React, { useState } from "react";
 
 export default function Home() {
+  const [showHighscore, setShowHighscore] = useState(false);
+
+  setTimeout(() => {
+    setShowHighscore(true);
+  }, 5000);
+
   return (
     <>
       <Head>
@@ -13,6 +20,7 @@ export default function Home() {
 
       <main>
         <GamePage />
+        {showHighscore && <div>HighScore</div>}
         <div className={styles.display}>
           <Display id="time" value="30" style="time" />
           <Display id="score" value="0" style="score" />
